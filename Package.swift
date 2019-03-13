d6353dfbd6c2a9487abe9553a88c612d49cef960
Cihat Gündüz <commits@cihatguenduez.de>
2019-03-13 11:46:40 +0100

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "WheelPicker",
+    products: [
+        .library(name: "WheelPicker", targets: ["WheelPicker"])
+    ],
+    targets: [
+        .target(
+            name: "WheelPicker"
+        )
+    ]
+)
