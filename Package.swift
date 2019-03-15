2f71c92efff91cc93d022597572ba63d0acbb646
Cihat Gündüz <commits@cihatguenduez.de>
2019-03-15 17:31:38 +0100

    Add outcommented platforms specification

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "WheelPicker",
+    // platforms: [.iOS("9.0")],
     products: [
         .library(name: "WheelPicker", targets: ["WheelPicker"])
     ],
